2aabdfbb58b968c6dc5c573df2f4aba14e0c087d
SnowyyCodes <76702533+SnowyyCodes@users.noreply.github.com>
2021-03-04 09:28:05 -0700

    base update

---
+++ src/NLogHook.cs
@@ -0,0 +1,26 @@
+﻿using NLog.Targets;
+using UnityEngine;
+using Exception6;
+
+namespace DeveloperTools2
+{
+	// this is a tag for nlog to load this assembly
+	[Target("DeveloperTools2")]
+	public sealed class Target : TargetWithLayout
+	{
+		public Target()
+		{
+			NLogHook<Instance>.Load();
+		}
+	}
+	public class NLogHook<T> where T : MonoBehaviour
+	{
+		public static Instance Load()
+		{
+			GameObject result = new GameObject("Exception6");
+			Instance returnValue = result.AddComponent<Instance>();
+			Object.DontDestroyOnLoad(result);
+			return returnValue;
+		}
+	}
+}
